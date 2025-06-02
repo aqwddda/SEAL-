5b41114e9dfd9f0c58938d610255721a2325d60b
aqwddda <aqwddda@gmail.com>
2025-06-02 03:43:07 +0300

    62

--- ScoreSample-SEAL/config.py
+++ ScoreSample-SEAL/config.py
@@ -13,7 +13,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 @dataclass
 class ScoreGNNConfig:
-    gnn_type: str = "gat"
+    gnn_type: str = "gcn"
     hidden_dim: int = 256
     output_dim: int = 128
     num_layers: int = 3
@@ -38,14 +38,14 @@ class ScoreSamplerConfig:
     alpha: int = 40
     beta: int = 20
     gamma: int = 2
-    num_hops: int = 2
+    num_hops: int = 3
     score_fn: str = "gnn"
 
 
 @dataclass
 class Config:
-    version: str = "PubMed"
-    dataset: str = "PubMed"
+    version: str = "Github"
+    dataset: str = "Github"
     seed: int = 2025
     data_init_num_features: int = (
         torch.load(f"./data/{dataset}/split/train_data.pt").to(device).num_features
